9a9bd3603a2c52cf79178e3c265ae92948ffd9a7
alejandro <alejandro.12w@gmail.com>
2024-01-26 01:41:59 -0800

    ..

--- src/models/SAMPLE_USERS.ts
+++ src/models/SAMPLE_USERS.ts
@@ -5,17 +5,17 @@ import { User } from '../models/user.model';
 export const SAMPLE_USERS: User[] = [  
   new User("user1", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer."), 
   
-  new User("user2", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "¡Ernest ha nacido para esto! DEBOOK cambia la industria del libro como lo hizo Spotify con la música!"),
+  new User("user2", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK cambia la industria del libro como lo hizo Spotify con la música!"),
   new User("user3", "assets/images/tomas_4.png", "Tomas Garnelo", "Social Entrepreneur", 680, "Ernest es la persona más creativa que conozco. Le sigo desde 2014 y se que es el proyecto de su vida."),
   new User("user4", "assets/images/alberto_6.png", "Alberto Rodilla", "Author & entrepreneur", 44, "El valor de aprender de los desafíos que Ernest enfrentará en tiempo real con DEBOOK es el nuevo MBA."),
-  new User("user5", "assets/images/carolina_3.png", "Carolina Millan", "Podcaster", 55, "Soy una lectora apasionada y sólo quiero que DEBOOK sea una realidad. Cambia las reglas del juego."),
+  new User("user5", "assets/images/carolina_3.png", "Carolina Millan", "Podcaster", 55, "Soy una lectora apasionada y sólo quiero que DEBOOK sea una realidad. Cambia las reglas."),
 
   new User("user6", "assets/images/david_1.png", "David Riudor", "Investor & founder", 455, "Confío en el potencial de Ernest como empresario y ejecutor. ¡Quiero ser parte de su sueño y comunidad!"),
 
   new User("user7", "assets/images/arya_7.png", "Arya  Anitza", "Book lover", 1, "¡La energía de esta comunidad es única! Desando conocera todos los emprendedores que serán parte!"),
-  new User("user8", "assets/images/ernest_8.png", "Ernest Viñas", "Disruptor", 1, "Como fundador soy el primero en invertir en mí mismo. Me compré 15 MAGICKEYS. Por ahora."),
+  new User("user8", "assets/images/ernest_8.png", "Ernest Viñas", "Disruptor", 1, "Como fundador soy el primero en invertir en mi visión. Me compré 15 MAGICKEYS. Por ahora."),
   
-  new User("user10", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "¡Ernest ha nacido para esto! DEBOOK cambia la industria del libro como lo hizo Spotify con la música!"),
+  new User("user10", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK cambia la industria del libro como lo hizo Spotify con la música!"),
   new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer."),
   new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer."),
 ];
