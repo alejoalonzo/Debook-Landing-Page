ce1090f2bdd63204dda8919d9e19edbecd00082f
alejandro <alejandro.12w@gmail.com>
2024-01-26 23:45:00 -0800

    DIST - flat

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -2,8 +2,10 @@ export const environment ={
 
     production: true,
 
-    baseHref: '/Debook-Landing-Page/',
+    baseHref: '/',
 
     apiUrl: 'https://datieskca7hlzshr3hgso4vacu0yfscv.lambda-url.us-east-1.on.aws/'
     
 }
+
+// baseHref: '/Debook-Landing-Page/',
