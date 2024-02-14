e243eb547c7289213f8a3c279c950621bbd251de
Jesse <mazureth@gmail.com>
2024-02-13 17:12:46 -0800

    Fixing CORS issue for environment variable so email form sumbmission works correctly.

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment ={
     apiUrl: 'https://datieskca7hlzshr3hgso4vacu0yfscv.lambda-url.us-east-1.on.aws/',
 
     // processEmailPhp: 'processEmail.php',
-    processEmailPhp: 'https://debookmagickey.com/processEmail.php',
+    processEmailPhp: '/processEmail.php',
     
     
 }
