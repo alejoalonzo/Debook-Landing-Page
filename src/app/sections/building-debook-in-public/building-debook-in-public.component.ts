cdfb8ed5f2e41cc386e8ac2f54d98e35f17cccb4
alejandro <alejandro.12w@gmail.com>
2024-02-17 21:54:46 -0800

    The 'Building Debook in Public' section has been redesigned, with new text in English and Spanish, new styles, and social media buttons added. It is working correctly on both mobile and desktop.

--- src/app/sections/building-debook-in-public/building-debook-in-public.component.ts
+++ src/app/sections/building-debook-in-public/building-debook-in-public.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-building-debook-in-public',
@@ -8,6 +8,46 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './building-debook-in-public.component.html',
   styleUrl: './building-debook-in-public.component.scss'
 })
-export class BuildingDebookInPublicComponent {
+export class BuildingDebookInPublicComponent implements OnInit{
+ 
+  currentLanguageOnBuildingDebook: string = 'es';
+  externalLinkPathYouTube: string = 'https://www.youtube.com/@Debook_esp';
+  linkedInLink: string = 'https://www.linkedin.com/in/ernestvinas/';
+
+
+  constructor(
+    private translate: TranslateService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getLangToLinks();
+  }
+
+  getLangToLinks(){
+    this.translate.onLangChange.subscribe(() => {
+      this.currentLanguageOnBuildingDebook = this.translate.currentLang;
+      this.updateLink();
+
+    });
+  }
+
+  updateLink() {
+    if (this.currentLanguageOnBuildingDebook === 'es') {
+      this.externalLinkPathYouTube = 'https://www.youtube.com/@Debook_esp';
+    } else if (this.currentLanguageOnBuildingDebook === 'en') {
+      this.externalLinkPathYouTube = 'https://www.youtube.com/@debook_eng'; 
+    }
+  }
+
+  navigateToLinkYouTube(){
+    const externalLinkYoutube = this.externalLinkPathYouTube;
+    window.open(externalLinkYoutube, '_blank');
+  }
+
+  navigateToLinkLinkedIn(){
+    const linkedInLink = this.linkedInLink;
+    window.open(linkedInLink, '_blank');
+  }
 
 }
