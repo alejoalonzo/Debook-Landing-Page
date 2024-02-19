8b5e1dc78ed03c441688139d431f7edcbfe5b079
Aditya <aditya@teebeds.com>
2024-02-19 10:03:01 +0530

    minor changes

--- src/app/sections/body/body.component.ts
+++ src/app/sections/body/body.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { WhatIsDebookComponent } from '../what-is-debook/what-is-debook.component';
 import { OneBookOneCommunityComponent } from '../one-book-one-community/one-book-one-community.component';
 import { CarrouselCardsComponent } from '../../components/carrousels/carrousel-cards/carrousel-cards.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [WhatIsDebookComponent, OneBookOneCommunityComponent, CarrouselCardsComponent],
+  imports: [
+    WhatIsDebookComponent,
+    OneBookOneCommunityComponent,
+    CarrouselCardsComponent,
+    TranslateModule,
+  ],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.scss'
+  styleUrl: './body.component.scss',
 })
-export class BodyComponent {
-
-}
+export class BodyComponent {}
