79553465ec3db455ebb57a47e3df5104a0fdf6ce
Aditya <aditya@teebeds.com>
2024-02-19 14:19:57 +0530

    Hero container added with image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,39 +20,42 @@ import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from './services/language.service';
 import { CarrouselCardsComponent } from './components/carrousels/carrousel-cards/carrousel-cards.component';
 import { SubMenuComponent } from './components/sub-menu/sub-menu.component';
-
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-              CommonModule, 
-              RouterOutlet, 
-              NavComponent, 
-              HeroComponent, 
-              BodyComponent, 
-              WhatIsDebookComponent,
-              FounderMessageComponent,
-              FundsGoToDevComponent,
-              PanelsComponent,
-              FaqsComponent,
-              TeamComponent,
-              FooterComponent,
-              BodyBottomComponent,
-              ProgressBarComponent,
-              CarrouselPrivilegesComponent,
-              CarrouselWHYBoxComponent,
-              CarrouselCardsComponent,
-              SubMenuComponent
-            ],
+    CommonModule,
+    RouterOutlet,
+    NavComponent,
+    HeroComponent,
+    BodyComponent,
+    WhatIsDebookComponent,
+    FounderMessageComponent,
+    FundsGoToDevComponent,
+    PanelsComponent,
+    FaqsComponent,
+    TeamComponent,
+    FooterComponent,
+    BodyBottomComponent,
+    ProgressBarComponent,
+    CarrouselPrivilegesComponent,
+    CarrouselWHYBoxComponent,
+    CarrouselCardsComponent,
+    SubMenuComponent,
+    CardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'debook';
 
-  constructor(private translate: TranslateService, private languageService: LanguageService){
-
+  constructor(
+    private translate: TranslateService,
+    private languageService: LanguageService
+  ) {
     // translate.setDefaultLang('en');
     translate.addLangs(['en', 'es']);
     translate.setDefaultLang('es');
@@ -60,9 +63,8 @@ export class AppComponent {
     const currentLang = this.languageService.getCurrentLang();
     translate.use(currentLang);
   }
-  switchLang(lang: string){
+  switchLang(lang: string) {
     this.translate.use(lang);
     this.languageService.setCurrentLang(lang);
   }
-
 }
