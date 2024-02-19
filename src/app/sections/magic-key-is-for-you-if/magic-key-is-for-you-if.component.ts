33bc827f7c5cf145f240a5c48acb6145ff368461
alejandro <alejandro.12w@gmail.com>
2024-02-18 20:08:37 -0800

    we-created-a-new-section DONE

--- src/app/sections/magic-key-is-for-you-if/magic-key-is-for-you-if.component.ts
+++ src/app/sections/magic-key-is-for-you-if/magic-key-is-for-you-if.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarrouselIsForYouIfComponent } from '../../components/carrousels/carrousel-is-for-you-if/carrousel-is-for-you-if.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-magic-key-is-for-you-if',
   standalone: true,
-  imports: [ CarrouselIsForYouIfComponent],
+  imports: [ CarrouselIsForYouIfComponent, TranslateModule],
   templateUrl: './magic-key-is-for-you-if.component.html',
   styleUrl: './magic-key-is-for-you-if.component.scss'
 })
