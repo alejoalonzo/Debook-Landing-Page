b079b67d7adfd4fe7d671a103890d67888169351
Aditya <aditya@teebeds.com>
2024-02-25 10:20:18 +0530

    Duplicacy from email reques modal removed and closable icon added

--- src/app/components/pop-up-email-request/pop-up-email-request.component.ts
+++ src/app/components/pop-up-email-request/pop-up-email-request.component.ts
@@ -1,5 +1,10 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, ViewChild, Input  } from '@angular/core';
-import { CommonModule,  } from '@angular/common';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  ViewChild,
+  Input,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { Dialog } from 'primeng/dialog';
@@ -11,45 +16,41 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { User } from '../../../models/user';
 import { DataSharingService } from '../../services/data-sharing.service';
 
-
 @Component({
   selector: 'app-pop-up-email-request',
   standalone: true,
   imports: [
-    DialogModule, 
-    ButtonModule, 
-    InputTextModule, 
-    FormsModule, 
-    CommonModule, 
-    ReactiveFormsModule, 
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
     TranslateModule,
-    ],
+  ],
   templateUrl: './pop-up-email-request.component.html',
   styleUrl: './pop-up-email-request.component.scss',
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PopUpEmailRequestComponent {
-
-
   @ViewChild('submitButton') submitButton: any;
   @Input() isDemo: boolean = true;
 
   @Input() popupButtonText: string = 'sections.funds-go-to-dev.interact';
   @Input() buttonClass: string = 'button-black-long';
-  
+
   currentLanguageOnPopoup: string = 'es';
 
   userForm!: FormGroup;
   isSubmitted!: boolean;
-  users: User[] =[];
+  users: User[] = [];
   visible: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private usersService: DataSharingService, 
+    private usersService: DataSharingService,
     private translate: TranslateService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this._initFormUser();
@@ -57,22 +58,22 @@ export class PopUpEmailRequestComponent {
   }
 
   showDialog() {
-      this.visible = true;
+    this.visible = true;
   }
   closeDialog() {
     this.visible = false;
   }
 
-  private _initFormUser(){
+  private _initFormUser() {
     this.userForm = this.formBuilder.group({
-      name:['', Validators.required],
-      email:['', [Validators.required, Validators.email]],
-    })
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
-  onSubmit(){
-    this.isSubmitted=true;
-    if(this.userForm.invalid){
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.userForm.invalid) {
       return;
     }
 
@@ -84,29 +85,23 @@ export class PopUpEmailRequestComponent {
       email: this.userForm.controls['email'].value,
       button: buttonType,
       language: language,
-    }
+    };
     console.log('usersData:', userFormData);
-    this._addUser(userFormData)
+    this._addUser(userFormData);
 
     this.userForm.disable();
-
   }
 
-
-  private _addUser(user: User){
-    
-    this.usersService.createNewContact(user).subscribe((user: User)=>{
-      console.log()
+  private _addUser(user: User) {
+    this.usersService.createNewContact(user).subscribe((user: User) => {
+      console.log();
     });
   }
 
-  getLangToForm(){
+  getLangToForm() {
     this.translate.onLangChange.subscribe(() => {
       this.currentLanguageOnPopoup = this.translate.currentLang;
       // console.log("langSus -> "+this.currentLanguageOnPopoup);
     });
   }
-
-
-
 }
