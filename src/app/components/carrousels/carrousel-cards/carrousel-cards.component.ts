dfa3cdd6b8a7cc760e4ee85a6f43976a38f6c5a5
alejandro <alejandro.12w@gmail.com>
2024-02-25 17:55:55 -0800

    A fifth element has been added to the carousel and the section title has also been changed in styles and text, On the other hand, we have a new dpi resolution in the header cards images, and they are now in English and Spanish

--- src/app/components/carrousels/carrousel-cards/carrousel-cards.component.ts
+++ src/app/components/carrousels/carrousel-cards/carrousel-cards.component.ts
@@ -1,9 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA,  Component, OnInit} from '@angular/core';
+import { ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { CardComponent } from '../../card/card.component';
 import { UsersService } from '../../../services/users.service';
+import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../../models/user.model';
 import { CommonModule } from '@angular/common';
+import Swiper from 'swiper';
+
+
 register();
 
 @Component({
@@ -14,20 +19,29 @@ register();
   styleUrls: ['./carrousel-cards.component.scss'],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
+
+
 export class CarrouselCardsComponent implements OnInit{
 
   sampleUsers: User[] = []; 
+  
+  @ViewChild('swiperContainer') swiperContainer?: Element;
 
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private translate: TranslateService) {}
 
   ngOnInit() {
     this.getSampleUsersList();
+    this.translate.onLangChange.subscribe(() => {
+      this.getSampleUsersList();
+    });
   }
+
 
   private getSampleUsersList() {
     this.usersService.getSampleUsers().subscribe(
       (users: User[]) => {
         this.sampleUsers = users;
+        // console.log(this.sampleUsers)
       }
     );
   }
--- src/models/SAMPLE_USERS.ts
+++ src/models/SAMPLE_USERS.ts
@@ -3,37 +3,37 @@
 import { User } from '../models/user.model';
 
 export const SAMPLE_USERS: User[] = [  
-  new User("user1", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer."), 
+  new User("user1", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer.", "PROPIETARIO"), 
   
-  new User("user2", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK cambia la industria del libro como lo hizo Spotify con la música!"),
-  new User("user3", "assets/images/tomas_4.png", "Tomas Garnelo", "Social Entrepreneur", 680, "Ernest es la persona más creativa que conozco. Le sigo desde 2014 y se que es el proyecto de su vida."),
-  new User("user4", "assets/images/alberto_6.png", "Alberto Rodilla", "Author & entrepreneur", 44, "El valor de aprender de los desafíos que Ernest enfrentará en tiempo real con DEBOOK es el nuevo MBA."),
-  new User("user5", "assets/images/carolina_3.png", "Carolina Millan", "Podcaster", 55, "Soy una lectora apasionada y sólo quiero que DEBOOK sea una realidad. Cambia las reglas."),
+  new User("user2", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK cambia la industria del libro como lo hizo Spotify con la música!", "PROPIETARIA"),
+  new User("user3", "assets/images/tomas_4.png", "Tomas Garnelo", "Social Entrepreneur", 680, "Ernest es la persona más creativa que conozco. Le sigo desde 2014 y se que es el proyecto de su vida.", "PROPIETARIO"),
+  new User("user4", "assets/images/alberto_6.png", "Alberto Rodilla", "Author & entrepreneur", 44, "El valor de aprender de los desafíos que Ernest enfrentará en tiempo real con DEBOOK es el nuevo MBA.", "PROPIETARIO"),
+  new User("user5", "assets/images/carolina_3.png", "Carolina Millan", "Podcaster", 55, "Soy una lectora apasionada y sólo quiero que DEBOOK sea una realidad. Cambia las reglas.", "PROPIETARIA"),
 
-  new User("user6", "assets/images/david_1.png", "David Riudor", "Investor & founder", 455, "Confío en el potencial de Ernest como empresario y ejecutor. ¡Quiero ser parte de su sueño y comunidad!"),
+  new User("user6", "assets/images/david_1.png", "David Riudor", "Investor & founder", 455, "Confío en el potencial de Ernest como empresario y ejecutor. ¡Quiero ser parte de su sueño y comunidad!", "PROPIETARIO"),
 
-  new User("user7", "assets/images/arya_7.png", "Arya  Anitza", "Book lover", 1, "¡La energía de esta comunidad es única! Desando conocera todos los emprendedores que serán parte!"),
-  new User("user8", "assets/images/ernest_8.png", "Ernest Viñas", "Disruptor", 1, "Como fundador soy el primero en invertir en mi visión. Me compré 15 MAGICKEYS. Por ahora."),
+  new User("user7", "assets/images/arya_7.png", "Arya  Anitza", "Book lover", 1, "¡La energía de esta comunidad es única! Desando conocera todos los emprendedores que serán parte!", "PROPIETARIA"),
+  new User("user8", "assets/images/ernest_8.png", "Ernest Viñas", "Disruptor", 1, "Como fundador soy el primero en invertir en mi visión. Me compré 15 MAGICKEYS. Por ahora.", "PROPIETARIO"),
   
-  new User("user10", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK cambia la industria del libro como lo hizo Spotify con la música!"),
-  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer."),
-  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer."),
+  new User("user10", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK cambia la industria del libro como lo hizo Spotify con la música!", "PROPIETARIA"),
+  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer.", "PROPIETARIO"),
+  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Ser autor ya no es un negocio. La industria del libro estaba muriendo. Ahora va a renacer.", "PROPIETARIO"),
 ];
 
 export const SAMPLE_USERS_EN: User[] = [  
-  new User("user1", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Being an author is not a business anymore. The book industry was dying. Now it is going to reborn."), 
+  new User("user1", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Being an author is not a business anymore. The book industry was dying. Now it is going to reborn.", "OWNER"), 
   
-  new User("user2", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK is going to change the book industry like Spotify did on the music one!"),
-  new User("user3", "assets/images/tomas_4.png", "Tomas Garnelo", "Social Entrepreneur", 680, "Ernest is the most creative person I know. I’ve been following him since 2014 and i know it is his life project."),
-  new User("user4", "assets/images/alberto_6.png", "Alberto Rodilla", "Author & entrepreneur", 44, "The value of learning from the challenges Ernest will face on real time with DEBOOK is the new MBA."),
-  new User("user5", "assets/images/carolina_3.png", "Carolina Millan", "Podcaster", 55, "I am a passionate reader and I just want DEBOOK to become a reality. It is going to be a game-changer."),
+  new User("user2", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK is going to change the book industry like Spotify did on the music one!", "OWNER"),
+  new User("user3", "assets/images/tomas_4.png", "Tomas Garnelo", "Social Entrepreneur", 680, "Ernest is the most creative person I know. I’ve been following him since 2014 and i know it is his life project.", "OWNER"),
+  new User("user4", "assets/images/alberto_6.png", "Alberto Rodilla", "Author & entrepreneur", 44, "The value of learning from the challenges Ernest will face on real time with DEBOOK is the new MBA.", "OWNER"),
+  new User("user5", "assets/images/carolina_3.png", "Carolina Millan", "Podcaster", 55, "I am a passionate reader and I just want DEBOOK to become a reality. It is going to be a game-changer.", "OWNER"),
 
-  new User("user6", "assets/images/david_1.png", "David Riudor", "Investor & founder", 455, "I trust Ernest as an enterpreneur and executor. I want to be part of his dream and community!"),
+  new User("user6", "assets/images/david_1.png", "David Riudor", "Investor & founder", 455, "I trust Ernest as an enterpreneur and executor. I want to be part of his dream and community!", "OWNER"),
 
-  new User("user7", "assets/images/arya_7.png", "Arya  Anitza", "Book lover", 1, "The energy around this community is unique and I can’t wait to meet all the entrepreneur that will be part of MK!"),
-  new User("user8", "assets/images/ernest_8.png", "Ernest Viñas", "Disruptor", 1, "As a founder I am the first to invest in myself. I bought for myself 15 MAGICKEYS. So far."),
+  new User("user7", "assets/images/arya_7.png", "Arya  Anitza", "Book lover", 1, "The energy around this community is unique and I can’t wait to meet all the entrepreneur that will be part of MK!", "OWNER"),
+  new User("user8", "assets/images/ernest_8.png", "Ernest Viñas", "Disruptor", 1, "As a founder I am the first to invest in myself. I bought for myself 15 MAGICKEYS. So far.", "OWNER"),
   
-  new User("user10", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK is going to change the book industry like Spotify did on the music one!"),
-  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Being an author is not a business anymore. The book industry was dying. Now it is going to reborn."),
-  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Being an author is not a business anymore. The book industry was dying. Now it is going to reborn."),
+  new User("user10", "assets/images/casandra_5.png", "Cassandra V.", "Founder & ex-google", 287, "DEBOOK is going to change the book industry like Spotify did on the music one!", "OWNER"),
+  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Being an author is not a business anymore. The book industry was dying. Now it is going to reborn.", "OWNER"),
+  new User("user9", "assets/images/jorge_2.png", "Jorge Branger", "Author & entrepreneur ", 122, "Being an author is not a business anymore. The book industry was dying. Now it is going to reborn.", "OWNER"),
 ];
