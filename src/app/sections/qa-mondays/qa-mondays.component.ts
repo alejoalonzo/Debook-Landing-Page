0c38c5939f598d5d3ca617a69442c49ea03fedc9
alejandro <alejandro.12w@gmail.com>
2024-03-17 20:17:56 -0700

    added Es-En

--- src/app/sections/qa-mondays/qa-mondays.component.ts
+++ src/app/sections/qa-mondays/qa-mondays.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-qa-mondays',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './qa-mondays.component.html',
   styleUrl: './qa-mondays.component.scss'
 })
